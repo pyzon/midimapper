6e477d34c41951a0c568964aab266b08ea05ea19
Kalló Kristóf <kristof.kallo@gmail.com>
2021-10-05 02:06:00 +0200

    cleanup comments

--- src/kristofkallo/midimapper/TrayMenu.java
+++ src/kristofkallo/midimapper/TrayMenu.java
@@ -10,8 +10,6 @@ public class TrayMenu {
 
     private final App app;
     private final TrayIcon trayIcon;
-    //private final MenuItem m400MenuItem;
-    //private final MenuItem loopMidiMenuItem;
 
     public TrayMenu(App app) throws FileNotFoundException, AWTException{
         this.app = app;
@@ -19,13 +17,6 @@ public class TrayMenu {
         MenuItem reconnectMenuItem = new MenuItem("Reconnect");
         reconnectMenuItem.addActionListener(e -> this.app.connectDevices());
         menu.add(reconnectMenuItem);
-        //Menu devicesMenu = new Menu("Devices");
-        //m400MenuItem = new MenuItem();
-        //m400MenuItem.setEnabled(false);
-        //loopMidiMenuItem = new MenuItem();
-        //loopMidiMenuItem.setEnabled(false);
-        //menu.add(m400MenuItem);
-        //menu.add(loopMidiMenuItem);
         MenuItem quitMenuItem = new MenuItem("Quit");
         quitMenuItem.addActionListener(e -> this.app.quit());
         menu.add(quitMenuItem);
